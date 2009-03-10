f115799232a91d9037c1d41630cadad6a3f20d0d
jhcasado <honorio.casado@aentos.es>
2009-03-10 15:39:13 +0000

    Improved rspec controller scaffold.

--- lib/generators/rspec_scaffold/templates/controller_spec.rb
+++ lib/generators/rspec_scaffold/templates/controller_spec.rb
@@ -6,25 +6,29 @@ describe <%= controller_class_name %>Controller do
     @mock_<%= file_name %> ||= mock_model(<%= class_name %>, stubs)
   end
 
-# as not logged
-  describe "as not logged" do
+# as logged with the correct role
+  describe "as authenticate" do
+
+    before(:each) do
+      login_as_scaffold_role
+    end
 
     describe "responding to GET index" do
 
       it "should expose all <%= table_name.pluralize %> as @<%= table_name.pluralize %>" do
-        <%= class_name %>.should_not_receive(:find).with(:all, {:limit=><%= class_name %>.per_page, :offset=>0, :order=>nil}).and_return([mock_<%= file_name %>])
+        <%= class_name %>.should_receive(:find).with(:all, {:limit=><%= class_name %>.per_page, :offset=>0, :order=>nil}).and_return([mock_<%= file_name %>])
         get :index
-        assigns[:<%= table_name %>].should_not == [mock_<%= file_name %>]
+        assigns[:<%= table_name %>].should == [mock_<%= file_name %>]
       end
 
       describe "with mime type of xml" do
 
         it "should render all <%= table_name.pluralize %> as xml" do
           request.env["HTTP_ACCEPT"] = "application/xml"
-          <%= class_name %>.should_not_receive(:find).with(:all).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
-          <%= file_name.pluralize %>.should_not_receive(:to_xml).and_return("generated XML")
+          <%= class_name %>.should_receive(:find).with(:all).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
+          <%= file_name.pluralize %>.should_receive(:to_xml).and_return("generated XML")
           get :index
-          response.body.should_not == "generated XML"
+          response.body.should == "generated XML"
         end
 
       end
@@ -34,19 +38,19 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET show" do
 
       it "should expose the requested <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
         get :show, :id => "37"
-        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
       describe "with mime type of xml" do
 
         it "should render the requested <%= file_name %> as xml" do
           request.env["HTTP_ACCEPT"] = "application/xml"
-          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_not_receive(:to_xml).and_return("generated XML")
+          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_receive(:to_xml).and_return("generated XML")
           get :show, :id => "37"
-          response.body.should_not == "generated XML"
+          response.body.should == "generated XML"
         end
 
       end
@@ -56,9 +60,9 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET new" do
 
       it "should expose a new <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_not_receive(:new).and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_receive(:new).and_return(mock_<%= file_name %>)
         get :new
-        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
     end
@@ -66,9 +70,9 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET edit" do
 
       it "should expose the requested <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
         get :edit, :id => "37"
-        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
     end
@@ -78,15 +82,15 @@ describe <%= controller_class_name %>Controller do
       describe "with valid params" do
 
         it "should expose a newly created <%= file_name %> as @<%= file_name %>" do
-          <%= class_name %>.should_not_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
+          <%= class_name %>.should_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
           post :create, :<%= file_name %> => {:these => 'params'}
-          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
         end
 
         it "should redirect to the created <%= file_name %>" do
           <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => true))
           post :create, :<%= file_name %> => {}
-          response.should_not redirect_to(<%= table_name.pluralize %>_path)
+          response.should redirect_to(<%= table_name.pluralize %>_path)
         end
 
       end
@@ -96,13 +100,13 @@ describe <%= controller_class_name %>Controller do
         it "should expose a newly created but unsaved <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
           post :create, :<%= file_name %> => {:these => 'params'}
-          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
         end
 
         it "should re-render the 'new' template" do
           <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => false))
           post :create, :<%= file_name %> => {}
-          response.should_not render_template('new')
+          response.should render_template('new')
         end
 
       end
@@ -114,21 +118,21 @@ describe <%= controller_class_name %>Controller do
       describe "with valid params" do
 
         it "should update the requested <%= file_name %>" do
-          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_not_receive(:update_attributes).with({'these' => 'params'})
+          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
         end
 
         it "should expose the requested <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
           put :update, :id => "1"
-          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
         end
 
         it "should redirect to the <%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
           put :update, :id => "1"
-          response.should_not redirect_to(<%= table_name.pluralize %>_path)
+          response.should redirect_to(<%= table_name.pluralize %>_path)
         end
 
       end
@@ -136,21 +140,21 @@ describe <%= controller_class_name %>Controller do
       describe "with invalid params" do
 
         it "should update the requested <%= file_name %>" do
-          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_not_receive(:update_attributes).with({'these' => 'params'})
+          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
         end
 
         it "should expose the <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
           put :update, :id => "1"
-          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
         end
 
         it "should re-render the 'edit' template" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
           put :update, :id => "1"
-          response.should_not render_template('edit')
+          response.should render_template('edit')
         end
 
       end
@@ -160,15 +164,15 @@ describe <%= controller_class_name %>Controller do
     describe "responding to DELETE destroy" do
 
       it "should destroy the requested <%= file_name %>" do
-        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
-        mock_<%= file_name %>.should_not_receive(:destroy)
+        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        mock_<%= file_name %>.should_receive(:destroy)
         delete :destroy, :id => "37"
       end
 
       it "should redirect to the <%= table_name %> list" do
         <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:destroy => true))
         delete :destroy, :id => "1"
-        response.should_not redirect_to(<%= table_name %>_url)
+        response.should redirect_to(<%= table_name %>_url)
       end
 
     end
@@ -349,30 +353,25 @@ describe <%= controller_class_name %>Controller do
 
   end
 
-  
-# as logged with the correct role
-  describe "as authenticate" do
-
-    before(:each) do
-      login_as_scaffold_role
-    end
+# as not logged
+  describe "as not logged" do
 
     describe "responding to GET index" do
 
       it "should expose all <%= table_name.pluralize %> as @<%= table_name.pluralize %>" do
-        <%= class_name %>.should_receive(:find).with(:all, {:limit=><%= class_name %>.per_page, :offset=>0, :order=>nil}).and_return([mock_<%= file_name %>])
+        <%= class_name %>.should_not_receive(:find).with(:all, {:limit=><%= class_name %>.per_page, :offset=>0, :order=>nil}).and_return([mock_<%= file_name %>])
         get :index
-        assigns[:<%= table_name %>].should == [mock_<%= file_name %>]
+        assigns[:<%= table_name %>].should_not == [mock_<%= file_name %>]
       end
 
       describe "with mime type of xml" do
 
         it "should render all <%= table_name.pluralize %> as xml" do
           request.env["HTTP_ACCEPT"] = "application/xml"
-          <%= class_name %>.should_receive(:find).with(:all).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
-          <%= file_name.pluralize %>.should_receive(:to_xml).and_return("generated XML")
+          <%= class_name %>.should_not_receive(:find).with(:all).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
+          <%= file_name.pluralize %>.should_not_receive(:to_xml).and_return("generated XML")
           get :index
-          response.body.should == "generated XML"
+          response.body.should_not == "generated XML"
         end
 
       end
@@ -382,19 +381,19 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET show" do
 
       it "should expose the requested <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
         get :show, :id => "37"
-        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
       end
 
       describe "with mime type of xml" do
 
         it "should render the requested <%= file_name %> as xml" do
           request.env["HTTP_ACCEPT"] = "application/xml"
-          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_receive(:to_xml).and_return("generated XML")
+          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_not_receive(:to_xml).and_return("generated XML")
           get :show, :id => "37"
-          response.body.should == "generated XML"
+          response.body.should_not == "generated XML"
         end
 
       end
@@ -404,9 +403,9 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET new" do
 
       it "should expose a new <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_receive(:new).and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_not_receive(:new).and_return(mock_<%= file_name %>)
         get :new
-        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
       end
 
     end
@@ -414,9 +413,9 @@ describe <%= controller_class_name %>Controller do
     describe "responding to GET edit" do
 
       it "should expose the requested <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
         get :edit, :id => "37"
-        assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
+        assigns[:<%= file_name %>].should_not equal(mock_<%= file_name %>)
       end
 
     end
@@ -426,15 +425,15 @@ describe <%= controller_class_name %>Controller do
       describe "with valid params" do
 
         it "should expose a newly created <%= file_name %> as @<%= file_name %>" do
-          <%= class_name %>.should_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
+          <%= class_name %>.should_not_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
           post :create, :<%= file_name %> => {:these => 'params'}
-          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
         end
 
         it "should redirect to the created <%= file_name %>" do
           <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => true))
           post :create, :<%= file_name %> => {}
-          response.should redirect_to(<%= table_name.pluralize %>_path)
+          response.should_not redirect_to(<%= table_name.pluralize %>_path)
         end
 
       end
@@ -444,13 +443,13 @@ describe <%= controller_class_name %>Controller do
         it "should expose a newly created but unsaved <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
           post :create, :<%= file_name %> => {:these => 'params'}
-          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
         end
 
         it "should re-render the 'new' template" do
           <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => false))
           post :create, :<%= file_name %> => {}
-          response.should render_template('new')
+          response.should_not render_template('new')
         end
 
       end
@@ -462,21 +461,21 @@ describe <%= controller_class_name %>Controller do
       describe "with valid params" do
 
         it "should update the requested <%= file_name %>" do
-          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
+          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_not_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
         end
 
         it "should expose the requested <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
           put :update, :id => "1"
-          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
         end
 
         it "should redirect to the <%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
           put :update, :id => "1"
-          response.should redirect_to(<%= table_name.pluralize %>_path)
+          response.should_not redirect_to(<%= table_name.pluralize %>_path)
         end
 
       end
@@ -484,21 +483,21 @@ describe <%= controller_class_name %>Controller do
       describe "with invalid params" do
 
         it "should update the requested <%= file_name %>" do
-          <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-          mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
+          <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+          mock_<%= file_name %>.should_not_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
         end
 
         it "should expose the <%= file_name %> as @<%= file_name %>" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
           put :update, :id => "1"
-          assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
+          assigns(:<%= file_name %>).should_not equal(mock_<%= file_name %>)
         end
 
         it "should re-render the 'edit' template" do
           <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
           put :update, :id => "1"
-          response.should render_template('edit')
+          response.should_not render_template('edit')
         end
 
       end
@@ -508,19 +507,20 @@ describe <%= controller_class_name %>Controller do
     describe "responding to DELETE destroy" do
 
       it "should destroy the requested <%= file_name %>" do
-        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-        mock_<%= file_name %>.should_receive(:destroy)
+        <%= class_name %>.should_not_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        mock_<%= file_name %>.should_not_receive(:destroy)
         delete :destroy, :id => "37"
       end
 
       it "should redirect to the <%= table_name %> list" do
         <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:destroy => true))
         delete :destroy, :id => "1"
-        response.should redirect_to(<%= table_name %>_url)
+        response.should_not redirect_to(<%= table_name %>_url)
       end
 
     end
 
   end
-  
+
+
 end
